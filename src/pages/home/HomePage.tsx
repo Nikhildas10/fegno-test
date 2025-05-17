afcb494d463a7e1c8520937e3cf748855f36cd39
Nikhildas10 <nikhildas1027@gmail.com>
2025-05-17 15:41:59 +0530

    fix: add margin top to heading in homepage

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -13,7 +13,7 @@ const Home = () => {
   }
 
   return (
-    <div className="flex-grow container mx-auto px-8 py-8">
+    <div className="flex-grow container mx-auto px-8 py-8 mt-15">
       <h1 className="text-2xl text-center font-bold mb-6">Products List</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {products.map((product) => (
