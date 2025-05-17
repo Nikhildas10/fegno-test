eed1e41f7190062cbff973f0809ec63d40331f80
Nikhildas10 <nikhildas1027@gmail.com>
2025-05-17 10:42:23 +0530

    chore: initialize react app with tailwind css,shadcn and tanstack

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Home from "./pages/home/HomePage";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <Home />
+    </>
+  );
+}
+
+export default App;
